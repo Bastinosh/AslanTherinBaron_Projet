7d37b87f805241c46999bfcec999dfe8b283531e
Aslan60 <muhammedaslan6068@gmail.com>
2025-09-29 18:45:14 +0200

    ajout de la logique du gestion de score

--- pong/script.js
+++ pong/script.js
@@ -127,44 +127,56 @@ function animateBall() {
   ball.positionY += ball.vitesseY;
   ball.positionX += ball.vitesseX;
 
-  if (ball.positionX + ball.width/2 >= canvas.width-1 || ball.positionX + ball.width/2 < 0) {
-    ball.positionX = canvas.width/2;
-    ball.positionY = canvas.height/2;
+  // Si la balle sort à droite = point pour le joueur gauche
+  if (ball.positionX + ball.width / 2 >= canvas.width - 1) {
+    score.left++;
+    ball.positionX = canvas.width / 2;
+    ball.positionY = canvas.height / 2;
   }
 
-  if (ball.positionY + ball.height/2 >= canvas.height-1) {
-    ball.positionY = canvas.height-ball.height; // Empêche l'objet de passer à travers
-    ball.vitesseY = -ball.vitesseY; // Inverse la direction et réduit la vitesse (perte d'énergie)
+  // Si la balle sort à gauche = point pour le joueur droit
+  if (ball.positionX + ball.width / 2 < 0) {
+    score.right++;
+    ball.positionX = canvas.width / 2;
+    ball.positionY = canvas.height / 2;
   }
 
-  if(ball.positionY + ball.height/2 < 0){
+  // Rebond en bas
+  if (ball.positionY + ball.height / 2 >= canvas.height - 1) {
+    ball.positionY = canvas.height - ball.height;
+    ball.vitesseY = -ball.vitesseY;
+  }
+
+  // Rebond en haut
+  if (ball.positionY + ball.height / 2 < 0) {
     ball.positionY = ball.height;
     ball.vitesseY = -ball.vitesseY;
   }
 
-  if(leftPaddle.x + leftPaddle.width/2 > ball.positionX - ball.width/2){
-    if(leftPaddle.y + leftPaddle.height <= ball.positionY - ball.height/2
-        || leftPaddle.y > ball.positionY + ball.height/2)
-    {
-      //perdu
-    }
-    else
-    {
+  // Collision raquette gauche
+  if (leftPaddle.x + leftPaddle.width / 2 > ball.positionX - ball.width / 2) {
+    if (
+      leftPaddle.y + leftPaddle.height <= ball.positionY - ball.height / 2 ||
+      leftPaddle.y > ball.positionY + ball.height / 2
+    ) {
+      // perdu
+    } else {
       ball.vitesseX = -ball.vitesseX;
     }
   }
 
-  if(rightPaddle.x - rightPaddle.width/2 < ball.positionX + ball.width/2){
-    if(rightPaddle.y + rightPaddle.height <= ball.positionY - ball.height/2
-        || rightPaddle.y > ball.positionY + ball.height/2)
-    {
-      //perdu
-    }
-    else
-    {
+  // Collision raquette droite
+  if (rightPaddle.x - rightPaddle.width / 2 < ball.positionX + ball.width / 2) {
+    if (
+      rightPaddle.y + rightPaddle.height <= ball.positionY - ball.height / 2 ||
+      rightPaddle.y > ball.positionY + ball.height / 2
+    ) {
+      // perdu
+    } else {
       ball.vitesseX = -ball.vitesseX;
     }
   }
 
-  drawBall()
+  drawBall();
 }
+
